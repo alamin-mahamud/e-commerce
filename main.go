00ef2712bd877bed0ff2e11927992328199368fa
Alamin <alamin.ineedahelp@gmail.com>
2018-11-05 20:49:15 +0600

    :zap: performance test

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/alamin-mahamud/e-commerce/cmd"
+import (
+	"github.com/alamin-mahamud/e-commerce/cmd"
+	"github.com/pkg/profile"
+)
 
 func main() {
+	defer profile.Start().Stop()
 	cmd.Execute()
 }
